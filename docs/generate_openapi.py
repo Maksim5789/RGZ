e5bd6b4a3e19ce9ea6688ad3e45d1e536c94f357
unknown <maksim.akishin.04@mail.ru>
2025-11-01 16:06:23 +0700

    Расчётно-графическое задание - Вариант №1: Flask API + OpenAPI + ReDoc CI/CD

---
+++ docs/generate_openapi.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import yaml
+from app import app
+
+def generate_openapi_yaml() -> None:
+    with app.test_client() as client:
+        resp = client.get('/apispec_1.json')
+        if resp.status_code == 200 and resp.is_json:
+            spec = resp.get_json()
+            with open("docs/openapi.yaml", "w", encoding="utf-8") as f:
+                yaml.dump(spec, f, allow_unicode=True, default_flow_style=False)
+            print("Спецификация OpenAPI сохранена в docs/openapi.yaml")
+        else:
+            raise SystemExit("Ошибка: не удалось получить спецификацию OpenAPI.")
+
+if __name__ == "__main__":
+    generate_openapi_yaml()
